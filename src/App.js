790ee60ae49021435c44c84d6a975d82ee9bd7c1
MykolaRak <natonrak@gmail.com>
2022-09-16 14:01:59 +0300

    new branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Characters from "./components/Characters";
+
 function App() {
   return (
     <div>
-      <h1>Hello les1</h1>
-      <Characters/>
+      <h1>Hello les2</h1>
     </div>
   );
 }
